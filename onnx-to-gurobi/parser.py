7390590d52a0965c0bd3be0e419a4ae17d510711
Muhammad Atallah <muhammad.sabri@outlook.com>
2024-10-24 16:39:18 +0200

    Parser updated

--- onnx-to-gurobi/parser.py
+++ onnx-to-gurobi/parser.py
@@ -1,47 +1,52 @@
 import onnx
+import numpy as np
 
 class ONNXParser:
+    def __init__(self, onnx_model_path):
+        self.onnx_model = onnx.load(onnx_model_path)
+        self.graph = self.onnx_model.graph
+        self.initializer_shapes = {}
+        self.initializer_values = {}
+        self.input_output_tensors_shapes = {}
+        self.nodes = []
+        self._parse_model()
 
-    def __init__(self, model_path):
-        self.model = onnx.load(model_path)
-        self.graph = self.model.graph
-        self.nodes = self.graph.node
-        self.initializers = self._load_initializers(self.graph)
-        self.inputs = self._load_inputs()
-        self.outputs = self._load_outputs()
+    def _parse_model(self):
+        # Fill initializer_shapes and initializer_values
+        for initializer in self.graph.initializer:
+            self.initializer_shapes[initializer.name] = onnx.numpy_helper.to_array(initializer).shape
+            self.initializer_values[initializer.name] = onnx.numpy_helper.to_array(initializer)
 
-    def _load_initializers(self, graph):
-        initializers = {}
-        for initializer in graph.initializer:
-            initializers[initializer.name] = onnx.numpy_helper.to_array(initializer)
-        return initializers
+        # Fill input_output_tensors_shapes
+        for input in self.graph.input:
+            self.input_output_tensors_shapes[input.name] = input.type.tensor_type.shape.dim[1].dim_value
 
-    def _load_inputs(self):
-        input_tensors = []
-        for input_tensor in self.graph.input:
-            input_tensors.append(self._parse_tensor(input_tensor))
-        return input_tensors
+        for output in self.graph.output:
+            self.input_output_tensors_shapes[output.name] = output.type.tensor_type.shape.dim[1].dim_value
 
-    def _load_outputs(self):
-        output_tensors = []
-        for output_tensor in self.graph.output:
-            output_tensors.append(self._parse_tensor(output_tensor))
-        return output_tensors
-
-    def _parse_tensor(self, tensor):
-        return {'tensor_name': tensor.name, 'tensor_shape': [dim.dim_value for dim in tensor.type.tensor_type.shape.dim]}
-
-    def get_graph(self):
-        return self.graph
-
-    def get_nodes(self):
-        return self.nodes
-
-    def get_initializers(self):
-        return self.initializers
-
-    def get_inputs(self):
-        return self.inputs
-
-    def get_outputs(self):
-        return self.outputs
+        # Form node objects
+        for node in self.graph.node:
+            inputs = []
+            for inp in node.input:
+                shape = self.initializer_shapes.get(inp, self.input_output_tensors_shapes.get(inp, 1))
+                inputs.append({'name': inp, 'shape': shape})
+            outputs = []
+            for out in node.output:
+                shape = self.initializer_shapes.get(out, self.input_output_tensors_shapes.get(out, 1))
+                outputs.append({'name': out, 'shape': shape})
+            attributes = []
+            for attribute in node.attribute:
+                if attribute.type == onnx.AttributeProto.FLOAT:
+                    value = attribute.f
+                elif attribute.type == onnx.AttributeProto.INT:
+                    value = attribute.i
+                else:
+                    value = None
+                attributes.append({'name': attribute.name, 'value': value})
+            self.nodes.append({
+                'name': node.name,
+                'type': node.op_type,
+                'input': inputs,
+                'output': outputs,
+                'attributes': attributes
+            })
