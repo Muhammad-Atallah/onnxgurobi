0f7a2bfa0ee7579de49ed00fb26a2598c196f02e
Muhammad Atallah <muhammad.sabri@outlook.com>
2024-11-03 16:01:53 +0100

    Gemm updated

--- onnx-to-gurobi/operators/gemm.py
+++ onnx-to-gurobi/operators/gemm.py
@@ -3,7 +3,6 @@ from gurobipy import quicksum
 
 class GemmOperator:
     def __init__(self, node, initializers):
-        print("inside init. Node: ", node["input"][0]["name"])
         self.input1 = node["input"][0]["name"]
         self.input2 = node["input"][1]["name"]
         self.input3 = node["input"][2]["name"] if len(node["input"]) > 2 else None
@@ -12,20 +11,26 @@ class GemmOperator:
 
     def apply_constraints(self, gurobi_model, variables):
         weights = self.initializers.get(self.input2)
-
         if weights is None:
             raise ValueError(f"Initializer for {self.input2} not found.")
 
-        bias = self.initializers.get(self.input3, np.zeros(weights.shape[0]))
+        bias = self.initializers.get(self.input3, np.zeros(weights.shape[1]))
 
         var_input = variables[self.input1]
         var_output = variables[self.output]
 
-        for i in range(len(var_input)):
-            if weights.ndim == 1:
-                weight_row = weights
-            else:
-                weight_row = weights[i]
+        gurobi_model.update()
 
-            expression = quicksum(float(weight_row[j]) * var_input[j] for j in range(len(var_input))) + float(bias[i])
+        input_size = len(var_input)
+        output_size = len(var_output)
+
+        # Checking if weights need to be transposed
+        if weights.shape != (input_size, output_size):
+            if weights.shape == (output_size, input_size):
+                weights = weights.T  # Transposing the weights
+            else:
+                raise ValueError(f"Unexpected weights shape {weights.shape}")
+
+        for i in range(output_size):
+            expression = quicksum(float(weights[j, i]) * var_input[j] for j in range(input_size)) + float(bias[i])
             gurobi_model.addConstr(var_output[i] == expression, name=f"Gemm_{self.output}_{i}")
