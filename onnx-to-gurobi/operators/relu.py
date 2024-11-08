f3fe9972396e84937a755b2f6c637877e65c54f5
Muhammad Atallah <muhammad.sabri@outlook.com>
2024-11-08 16:17:23 +0100

    relu updated

--- onnx-to-gurobi/operators/relu.py
+++ onnx-to-gurobi/operators/relu.py
@@ -2,19 +2,18 @@ from gurobipy import GRB
 
 class ReLUOperator:
     def __init__(self, node, initializers):
-        self.input = node.input[0]
-        self.output = node.output[0]
+        self.input = node["input"][0]["name"]
+        self.output = node["output"][0]["name"]
 
     def apply_constraints(self, gurobi_model, variables):
         var_input = variables[self.input]
         var_output = variables[self.output]
+        binary_var = variables[f"relu_binary_{self.output}"]
         upper_bound = 1e10
 
-        binary_var = gurobi_model.addVars(var_input.keys(), vtype=GRB.BINARY, name=f"relu_binary_{self.output}")
-
-        for idx in var_input.keys():
-            gurobi_model.addConstr(var_output[idx] >= var_input[idx])
-            gurobi_model.addConstr(var_output[idx] >= 0)
-            gurobi_model.addConstr(var_output[idx] <= upper_bound)
-            gurobi_model.addConstr(var_output[idx] <= var_input[idx] + upper_bound * (1 - binary_var[idx]))
-            gurobi_model.addConstr(var_output[idx] <= upper_bound * binary_var[idx])
+        for i in var_input.keys():
+            gurobi_model.addConstr(var_output[i] >= var_input[i])
+            gurobi_model.addConstr(var_output[i] >= 0)
+            gurobi_model.addConstr(var_output[i] <= upper_bound)
+            gurobi_model.addConstr(var_output[i] <= var_input[i] + upper_bound * (1 - binary_var[i]))
+            gurobi_model.addConstr(var_output[i] <= upper_bound * binary_var[i])
