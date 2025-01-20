15b7484de7a721a3caa48776699442a83e159a06
Muhammad Atallah <muhammad.sabri@outlook.com>
2025-01-20 16:10:04 +0100

    updates

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 import onnxruntime as ort
 from gurobipy import GRB
+from onnx_to_gurobi import ONNXToGurobi
 
 def run_onnx_model(model_path, input_data):
     """
